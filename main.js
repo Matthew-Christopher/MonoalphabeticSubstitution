869415cea23727e0f43095c1cd1d420067295a22
Matthew Christopher <contact.matthew.christopher@gmail.com>
2017-11-13 19:31:49 +0000

    Bug fixes

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let solved = [];
+let ciphertxt = '';
 
 function clear(btn) {
   document.getElementsByName('plain_' + btn.toLowerCase())[0].value = '';
 }
 
 function replace() {
-  document.getElementsByName('ciphertext')[0].value = ciphertxt;
-  let temp = ciphertxt;
+  let ciphtemp = ciphertxt;
   for (let i=0; i<alphabet.length; i++) {
     let re = new RegExp(alphabet[i], 'g');
-    if (document.getElementsByName('plain_' + alphabet[i])[0].value)
-      temp = temp.replace(re, document.getElementsByName('plain_' + alphabet[i])[0].value);
-    document.getElementsByName('ciphertext')[0].value = temp;
+    if (document.getElementsByName('plain_' + alphabet[i])[0].value) {
+      ciphtemp = ciphtemp.replace(re, document.getElementsByName('plain_' + alphabet[i])[0].value.toLowerCase());
+      cipherinput(null, -1);
+    }
+    document.getElementsByName('ciphertext')[0].value = ciphtemp;
   }
 }
 
@@ -37,17 +40,46 @@ function down(event, field) {
     event.preventDefault();
   } else {
     setTimeout(() => {
-      replace(field.name.substr(-1), field.value.toLowerCase());
+      replace();
     }, 0);
   }
 }
 
-function cipherinput() {
-  setTimeout(() => {
-    ciphertxt = document.getElementsByName('ciphertext')[0].value.toUpperCase().replace(/\s/g, '').replace(/[.,\/'"+@#!$%\^&\*;:{}=\-_`~()]/g, "");
-    document.getElementsByName('ciphertext')[0].value = document.getElementsByName('ciphertext')[0].value.toUpperCase().replace(/\s/g, '').replace(/[.,\/'"+@#!$%\^&\*;:{}=\-_`~()]/g, "");
-    replace();
-  }, 0);
+function cipherinput(event, code) {
+  if (event == null || ((event.keyCode || event.which) > 64 && (event.keyCode || event.which) < 91 && !event.ctrlKey)) {
+    if (event != null) {
+      event.preventDefault();
+      document.getElementsByName('ciphertext')[0].value += String.fromCharCode(event.keyCode || event.which).toUpperCase();
+      ciphertxt += String.fromCharCode(event.keyCode || event.which).toUpperCase();
+    }
+    setTimeout(() => {
+      temp = document.getElementsByName('ciphertext')[0].value.split("");
+      for (let i=0; i<document.getElementsByName('ciphertext')[0].value.length; i++) {
+        if (temp[i] == temp[i].toUpperCase()) {
+          solved[i] = false;
+        } else {
+          solved[i] = true;
+        }
+      }
+      for (let i=0; i<document.getElementsByName('ciphertext')[0].value.length; i++) {
+        if (!solved[i]) {
+          temp[i] = temp[i].toUpperCase();
+        } else {
+          temp[i] = temp[i].toLowerCase();
+        }
+      }
+      console.log(solved);
+      console.log(temp);
+      document.getElementsByName('ciphertext')[0].value = temp.join("").replace(/\s/g, '').replace(/[.,\/'"+@#!$%\^&\*;:{}=\-_`~()]/g, "");
+      console.log("Ciphertxt: " + ciphertxt);
+      if (event != null)
+        replace();
+    }, 0);
+  } else {
+    setTimeout(() => {
+      document.getElementsByName('ciphertext')[0].value = document.getElementsByName('ciphertext')[0].value.replace(/\s/g, '').replace(/[.,\/'"+@#!$%\^&\*;:{}=\-_`~()]/g, "");
+    });
+  }
 }
 
 let observe;
